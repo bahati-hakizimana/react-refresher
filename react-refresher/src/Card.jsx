8d7c733196c6a8f321277ad3a786bdaefffcd38d
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2025-06-02 01:36:10 -0700

    useEffect Refresher with in counter

--- react-refresher/src/Card.jsx
+++ react-refresher/src/Card.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Card = ({ title }) => {
     const [ hasLiked, setHasLiked] = useState(false);
+    const [count, setCout] = useState(0);
+  useEffect(() =>{
+    console.log(`${title}, has been liked: ${hasLiked}`);
+  }, [hasLiked]);
+
+  useEffect(() =>{
+   console.log("Card Rendered!");
+   
+  }, []);
   return (
-    <div className="card">
-      <h1>{title}</h1>
+    <div className="card" onClick={() => setCout( count +1)}>
+      <h1>{title} <br />{count || null}</h1>
+     
      <button onClick={() => setHasLiked(!hasLiked)}>
         { hasLiked ? 'Liked' : 'Like' }
      </button>
